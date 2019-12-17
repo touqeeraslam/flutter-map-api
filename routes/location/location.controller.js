4e6324fcc1759440c53abdbdc2530ce68484686f
touqeer <touqeer>
2019-12-17 19:02:49 +0500

    changed the value of lat and long for map

--- routes/location/location.controller.js
+++ routes/location/location.controller.js
@@ -5,8 +5,8 @@ var controller = {}
 controller.get_coords = function (req, res) {
     //creating sample lat and long
     var coordinate = {
-        lat: 92.33,
-        long: 93.145
+        lat: 37.42796133580664,
+        long: -122.085749655962
     }
     //sending response back 
     res.send(coordinate);
