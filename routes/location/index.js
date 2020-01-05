09ef5c2448b483e4f26dc1c4302af2e14765004c
touqeer <touqeer>
2020-01-05 13:54:00 +0500

    added a route

--- routes/location/index.js
+++ routes/location/index.js
@@ -4,5 +4,9 @@ var locationController = require('./location.controller')
 
 /* GET lat and long. */
 router.get('/',locationController.get_coords );
+router.post('/login',function(req,res){
+    console.log(req.body);
+    return res.send(req.body);
+});
 
 module.exports = router;
